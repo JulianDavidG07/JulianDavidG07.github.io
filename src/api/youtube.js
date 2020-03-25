43b4284b3bd01cdedb4818587811b9792f612e54
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-03-24 19:06:18 -0500

    [UPDATE] comments update

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-/* baseURL from which we will consume the Yotube API through axios*/
+/* baseURL from which we will consume the Yotube API through axios */
 export default axios.create(
     {baseURL: 'https://www.googleapis.com/youtube/v3/'}
     );
